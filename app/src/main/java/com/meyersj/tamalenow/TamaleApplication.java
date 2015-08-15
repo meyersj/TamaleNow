5a815b3d709b72deacc83ae7656df0066c578114
Jeffrey Meyers <meyers2@pdx.edu>
2015-08-14 20:15:56 -0700

    first commit of app

---
+++ app/src/main/java/com/meyersj/tamalenow/TamaleApplication.java
@@ -0,0 +1,68 @@
+package com.meyersj.tamalenow;
+
+import android.app.Application;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import com.meyersj.tamalenow.utilities.PropKeys;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class TamaleApplication extends Application {
+
+    private static TamaleApplication singleton;
+    private Properties properties;
+
+    public TamaleApplication getInstance() {
+        return singleton;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        singleton = this;
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    private void readProperties() {
+        Resources resources = this.getResources();
+        AssetManager assetManager = resources.getAssets();
+
+        // read properties from /assets directory
+        try {
+            InputStream inputStream = assetManager.open("config.properties");
+            properties = new Properties();
+            properties.load(inputStream);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public String getToken() {
+        return properties.getProperty(PropKeys.MAPBOX_TOKEN, "");
+    }
+
+    public String getMapID() {
+        return properties.getProperty(PropKeys.MAP_ID, "");
+    }
+
+}
---
+++ app/src/main/java/com/meyersj/tamalenow/utilities/PropKeys.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2015 Jeffrey Meyers
+ *
+ * This program is released under the "MIT License".
+ * Please see the file COPYING in this distribution for license terms.
+ */
+
+package com.meyersj.tamalenow.utilities;
+
+
+public class PropKeys {
+
+    public static final String MAPBOX_TOKEN = "mapbox_token";
+    public static final String MAP_ID = "map_id";
+
+}
