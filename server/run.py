e5228b665051d686b2f36a2b5016aa81259ed93d
Jeffrey Meyers <meyers2@pdx.edu>
2015-08-15 21:11:22 -0700

    add copyright notice

--- server/run.py
+++ server/run.py
@@ -2,6 +2,9 @@
 
 # Copyright (C) 2015 Jeffrey Meyers
 
+# This program is released under the "MIT License".
+# Please see the file COPYING in this distribution for
+# license terms.
 
 from app import app
 app.run(debug=True)
