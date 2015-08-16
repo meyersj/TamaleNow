cc315ad66a9c30f44c3ebb195f52d0cde6e4e992
Jeffrey Meyers <meyers2@pdx.edu>
2015-08-15 19:47:01 -0700

    update active table to record timestamps

--- server/sql/create.sql
+++ server/sql/create.sql
@@ -1,11 +1,17 @@
 -- Copyright (C) 2015 Jeffrey Meyers
+--
+-- This program is released under the "MIT License".
+-- Please see the file COPYING in this distribution for
+-- license terms.
 
 BEGIN;
 
 DROP TABLE IF EXISTS active;
 CREATE TABLE active (
-    vendor_id VARCHAR PRIMARY KEY,
-    active BOOLEAN
+    vendor_id VARCHAR NOT NULL,
+    tstamp TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    active BOOLEAN,
+    PRIMARY KEY (vendor_id, tstamp)
 );
 
 DROP TABLE IF EXISTS locations;
